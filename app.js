e46f18c5abcf9aab4e217764c8cec38f057e9773
Katu97 <skatuchin.97.sk@gmail.com>
2022-04-17 13:34:25 -0300

    mejora botones

---
+++ app.js
@@ -0,0 +1,13 @@
+function encriptarMensaje(){
+    let mensaje = document.getElementById("textarea");
+
+    for(i = 0; i < mensaje.lenght; i++){
+        
+    }
+
+    console.log(mensaje.value.toString());
+}
+
+function desencriptarMensaje(){
+
+}
