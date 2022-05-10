791650bf31b40d75b9e97e0128e201c09f57ae4d
Katu97 <skatuchin.97.sk@gmail.com>
2022-05-10 02:37:24 -0300

    mejoras js y css

--- app.js
+++ app.js
@@ -1,3 +1,68 @@
+// Funcion encriptar
+function encriptarMensaje(){
+    let texto = document.getElementById("textarea").value; // .value.toLowerCase(); hacer las letras minusculas
+    
+    mensajeCorrecto(texto);
+
+    if(texto == ""){
+        nohaymensaje();
+    } else if(bandera == false){
+        alert("Se debe ingresar texto en minúscula y sin acentos");
+        cantLetras = 0;
+    } else {
+        let textoEncriptado = texto.replace(/e/igm, "enter");
+        textoEncriptado = textoEncriptado.replace(/i/igm, "imes");
+        textoEncriptado = textoEncriptado.replace(/a/igm, "ai");
+        textoEncriptado = textoEncriptado.replace(/o/igm, "ober");
+        textoEncriptado = textoEncriptado.replace(/u/igm, "ufat");
+
+        mostrarmensaje(textoEncriptado);
+        document.getElementById("textarea").value = "";
+        bandera = false;
+        cantLetras = 0;
+    }
+  
+}
+
+function desencriptarMensaje(){
+    let texto = document.getElementById("textarea").value;
+    mensajeCorrecto(texto);
+
+    if(texto == ""){
+        nohaymensaje();
+    } else if(bandera == false){
+        alert("Se debe ingresar texto en minúscula y sin acentos");
+        cantLetras = 0;
+    } else {
+        let textodesencriptado = texto.replace(/ai/igm, "a");
+        textodesencriptado = textodesencriptado.replace(/enter/igm, "e");
+        textodesencriptado = textodesencriptado.replace(/imes/igm, "i");
+        textodesencriptado = textodesencriptado.replace(/ober/igm, "o");
+        textodesencriptado = textodesencriptado.replace(/ufat/igm, "u");
+
+        mostrarmensaje(textodesencriptado);
+        document.getElementById("textarea").value = "";
+        bandera = false;
+        cantLetras = 0;
+    }
+
+}  
+
+//Funcion que evalua que se cumplan las condiciones de escritura
+function mensajeCorrecto(mensaje){
+    let mensajeLetras = Array.from(mensaje);
+    for (let i = 0; i < mensajeLetras.length; i++){
+        if((mensajeLetras[i].charCodeAt(0) >= 97 && mensajeLetras[i].charCodeAt(0) <= 122) || (mensajeLetras[i].charCodeAt(0) == 32) || (mensajeLetras[i].charCodeAt(0) == 164)){
+            cantLetras = cantLetras + 1;
+        }
+    }
+
+    if(cantLetras == mensajeLetras.length){
+        bandera = true;
+    }
+}
+
+// Funcion que borra alertas y muestra mensaje encriptado o desencriptado
 function mostrarmensaje(mensaje){
     console.log(mensaje);
 
@@ -9,48 +74,22 @@ function mostrarmensaje(mensaje){
     document.getElementById("mensajeprocesado").innerHTML = mensaje;
 }
 
-function encriptarMensaje(){
-    let texto = document.getElementById("textarea").value.toLowerCase();
-    
-    if(texto != ""){
-        let textoEncriptado = texto.replace(/e/igm, "enter");
-        textoEncriptado = textoEncriptado.replace(/i/igm, "imes");
-        textoEncriptado = textoEncriptado.replace(/a/igm, "ai");
-        textoEncriptado = textoEncriptado.replace(/o/igm, "ober");
-        textoEncriptado = textoEncriptado.replace(/u/igm, "ufat");
-
-        mostrarmensaje(textoEncriptado);
-        document.getElementById("textarea").value = "";
-    } else {
-        alert("No se ha ingresado un mensaje");
-    }
-  
-}
-
-function desencriptarMensaje(){
-    let texto = document.getElementById("textarea").value.toLowerCase();
-
-    if (texto != ""){
-        let textodesencriptado = texto.replace(/ai/igm, "a");
-        textodesencriptado = textodesencriptado.replace(/enter/igm, "e");
-        textodesencriptado = textodesencriptado.replace(/imes/igm, "i");
-        textodesencriptado = textodesencriptado.replace(/ober/igm, "o");
-        textodesencriptado = textodesencriptado.replace(/ufat/igm, "u");
-
-        mostrarmensaje(textodesencriptado);
-        document.getElementById("textarea").value = "";
-    } else {
-        alert("No se ha ingresado un mensaje");
-    }
-    
-}   
-
 function copiarMensaje(){
     let mensajeProcesado = document.getElementById("mensajeprocesado").innerHTML;
     navigator.clipboard.writeText(mensajeProcesado);
     alert("Mensaje copiado!");
 }
 
+function nohaymensaje(){
+    document.getElementById("borrar1").style.display = "initial";
+    document.getElementById("borrar2").style.display = "initial";
+    document.getElementById("borrar3").style.display = "initial";
+    document.getElementById("aparecer").style.display = "none";
+    document.getElementById("botoncopiar").style.display = "none";
+}
+
+let cantLetras = 0;
+let bandera = false;
 let texto = document.getElementById("textarea").value.toLowerCase();
 let mensajeProcesado = document.getElementById("mensajeprocesado");
 let botonEncriptar = document.getElementById("encriptar");
