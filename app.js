a73f3ed014003922e669ff16aa147e33c04c1ccd
Katu97 <skatuchin.97.sk@gmail.com>
2022-05-09 23:32:24 -0300

    modificaciones js

--- app.js
+++ app.js
@@ -1,13 +1,54 @@
+function mostrarmensaje(mensaje){
+    console.log(mensaje);
+
+    document.getElementById("borrar1").style.display = "none";
+    document.getElementById("borrar2").style.display = "none";
+    document.getElementById("borrar3").style.display = "none";
+    document.getElementById("aparecer").style.display = "block";
+    document.getElementById("botoncopiar").style.display = "block";
+    document.getElementById("mensajeprocesado").innerHTML = mensaje;
+}
+
 function encriptarMensaje(){
-    let mensaje = document.getElementById("textarea");
-
-    for(i = 0; i < mensaje.lenght; i++){
+    let texto = document.getElementById("textarea").value.toLowerCase();
         
-    }
+    let textoEncriptado = texto.replace(/e/igm, "enter");
+    textoEncriptado = textoEncriptado.replace(/i/igm, "imes");
+    textoEncriptado = textoEncriptado.replace(/a/igm, "ai");
+    textoEncriptado = textoEncriptado.replace(/o/igm, "ober");
+    textoEncriptado = textoEncriptado.replace(/u/igm, "ufat");
 
-    console.log(mensaje.value.toString());
+    mostrarmensaje(textoEncriptado);
+    document.getElementById("textarea").value = "";
+    document.getElementById("textarea").focus();
 }
 
 function desencriptarMensaje(){
+    let texto = document.getElementById("textarea").value.toLowerCase();
 
+    let textodesencriptado = texto.replace(/ai/igm, "a");
+    textodesencriptado = textodesencriptado.replace(/enter/igm, "e");
+    textodesencriptado = textodesencriptado.replace(/imes/igm, "i");
+    textodesencriptado = textodesencriptado.replace(/ober/igm, "o");
+    textodesencriptado = textodesencriptado.replace(/ufat/igm, "u");
+
+    mostrarmensaje(textodesencriptado);
+    document.getElementById("textarea").value = "";
+    document.getElementById("textarea").focus();
+}   
+
+function copiarMensaje(){
+    let mensajeProcesado = document.getElementById("mensajeprocesado").innerHTML;
+    navigator.clipboard.writeText(mensajeProcesado);
+    alert("Mensaje copiado!");
 }
+
+let texto = document.getElementById("textarea").value.toLowerCase();
+let mensajeProcesado = document.getElementById("mensajeprocesado");
+let botonEncriptar = document.getElementById("encriptar");
+let botonDesencriptar = document.getElementById("desencriptar");
+let botonCopiar = document.getElementById("botoncopiar");
+
+botonEncriptar.onclick = encriptarMensaje;
+botonDesencriptar.onclick = desencriptarMensaje;
+botonCopiar.onclick = copiarMensaje;
